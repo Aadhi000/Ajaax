0652b9fff66e428a590b6dff623a6c70125e70f1
ααdhí <87893573+Aadhi000@users.noreply.github.com>
2023-03-29 06:24:27 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Joelkb/DQ-the-file-donor.git /DQ-The-File-Donor
+  git clone https://github.com/Aadhi000/Ajaax.git /Ajaax
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /DQ-The-File-Donor
+  git clone $UPSTREAM_REPO /Ajaax
 fi
-cd /DQ-The-File-Donor
+cd /Ajaax
 pip3 install -U -r requirements.txt
 echo "Startin...."
 python3 bot.py
